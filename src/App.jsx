97230e7e514bceb6d707740806917dcd3a52a53c
Samuel Paget <sjapaget@gmail.com>
2022-11-03 18:21:54 +0100

    Add event handler to start the quiz

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import Start from './Start';
 import Quiz from './Quiz'
 
 function App() {
+
+  const [quizBegun, setQuizBegun] = useState(false);
+
+  function startQuiz() {
+    setQuizBegun(prev => !prev)
+  }
   return (
     <div className='h-screen flex flex-col justify-center items-center bg-bgBeige-500'>
-      <Quiz />
+      {quizBegun
+        ? <Quiz />
+        : <Start startQuiz={startQuiz} />
+      }
     </div>
   )
 }
--- src/Start.jsx
+++ src/Start.jsx
@@ -1,6 +1,7 @@
 import { React } from "react";
 
-function Start() {
+function Start(props) {
+  const { startQuiz } = props;
   return (
     <div className="text-center">
       <h1 className="py-4 text-5xl">
@@ -9,7 +10,10 @@ function Start() {
       <p className="py-4 text-xl">
         Want to test your literature knowledge? Look no further!
       </p>
-      <button className="my-8 p-4 rounded-lg text-bgBeige-500 text-xl bg-btnBlue-500 hover:bg-btnBlue-500/75">
+      <button
+        className="my-8 p-4 rounded-lg text-bgBeige-500 text-xl bg-btnBlue-500 hover:bg-btnBlue-500/75"
+        onClick={startQuiz}
+      >
         Start quiz
       </button>
     </div>
