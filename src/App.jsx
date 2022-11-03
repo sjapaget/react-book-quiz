d82739ea0f1846c968a409b784b9e3b26b97d4e6
Samuel Paget <sjapaget@gmail.com>
2022-11-03 18:16:38 +0100

    Add Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { React, useState } from 'react';
 import Start from './Start';
+import Quiz from './Quiz'
 
 function App() {
   return (
     <div className='h-screen flex flex-col justify-center items-center bg-bgBeige-500'>
-      <Start />
+      <Quiz />
     </div>
   )
 }
---
+++ src/Quiz.jsx
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+function Quiz() {
+  return (
+    <div>
+      <h1>quiz here</h1>
+    </div>
+  )
+}
+
+export default Quiz;
